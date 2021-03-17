2f2e34dec0df630f158bea930544ad49a56a7425
GPN-Phat <phat@gpn.com.vn>
2021-03-17 17:07:18 +0700

    Add project

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] List<GameObject> MobsTemplate;
+    List<Mobs> Mobs;
+    float Timer = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateMobs();
+    }
+
+    void GenerateMobs()
+    {
+        Mobs = new List<Mobs>();
+        for (int i = 0; i < MobsTemplate.Count; i++)
+        {
+            GameObject newMob = Instantiate(MobsTemplate[i], GameObject.FindGameObjectWithTag("mobs").transform);
+            newMob.transform.position = new Vector2(i * 100, i * 100);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeCountdown();
+        TouchCount();
+    }
+
+    void TimeCountdown()
+    {
+        Timer -= Time.deltaTime;
+        if (Timer < 0)
+        {
+            Debug.Log("TIME OUT!");
+        }
+    }
+
+    void TouchCount()
+    {
+        if (Input.touchCount > 0)
+        {
+            Debug.Log(Input.touchCount);
+        }
+    }
+}
---
+++ Assets/Scripts/Mobs.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mobs : MonoBehaviour
+{
+    Vector2 position;
+
+    public Mobs(Vector2 _position)
+    {
+        position = _position;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
